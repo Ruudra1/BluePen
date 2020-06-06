c69dfdd97c937c3823e3accec62cae1633f6ea61
jayvora9 <51924161+jayvora9@users.noreply.github.com>
2020-06-06 13:19:48 +0530

    blogs index.php working

--- src/new_blog/index.php
+++ src/new_blog/index.php
@@ -5,49 +5,12 @@ $sql="SELECT * FROM blog ORDER BY id desc";
 $result = mysqli_query($conn, $sql);
   
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <title>BluePen-Blogs</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <link href="https://fonts.googleapis.com/css?family=Muli:300,400,700|Playfair+Display:400,700,900" rel="stylesheet">
-
-    <link rel="stylesheet" href="fonts/icomoon/style.css">
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/magnific-popup.css">
-    <link rel="stylesheet" href="css/jquery-ui.css">
-    <link rel="stylesheet" href="css/owl.carousel.min.css">
-    <link rel="stylesheet" href="css/owl.theme.default.min.css">
-    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
-    <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
-    <link rel="stylesheet" href="css/aos.css">
-
-    <link rel="stylesheet" href="css/style.css">
-  </head>
-  <body>
-  
-  <div class="site-wrap"  > 
-
-    <div class="site-mobile-menu">
-      <div class="site-mobile-menu-header">
-        <div class="site-mobile-menu-close mt-3">
-          <span class="icon-close2 js-menu-toggle"></span>
-        </div>
-      </div>
-      <div class="site-mobile-menu-body"></div>
-    </div>
-    <br><br>
-    <h1 class="text-center">Blue Pen Blogs</h1>
-    <br><br>
-    
-
-   
 <?php
-$a=12;
-echo '
+
+echo '<br><br>
+<h1 class="text-center" style="margin-top:9%;">Blue Pen Blogs</h1>
+<br><br>
+
     <div class="site-section">
       <div class="container">
         <div class="row mb-5">
@@ -62,9 +25,7 @@ echo '
           $row1=mysqli_fetch_array($result1);
 
             $tag=explode(";",$row['tags']);
-            if($a==0)
-        break;
-        $a--;
+            
         echo '  <div class="col-lg-4 mb-4">
             <div class="entry2">
               <a href="single.php?id='.$row['id'].'"><img src="../blog/'.$row['main'].'" alt="Image" class="img-fluid rounded"></a>
@@ -96,23 +57,7 @@ echo '
   </div>
 </div>
 
-  <script src="js/jquery-3.3.1.min.js"></script>
-  <script src="js/jquery-migrate-3.0.1.min.js"></script>
-  <script src="js/jquery-ui.js"></script>
-  <script src="js/popper.min.js"></script>
-  <script src="js/bootstrap.min.js"></script>
-  <script src="js/owl.carousel.min.js"></script>
-  <script src="js/jquery.stellar.min.js"></script>
-  <script src="js/jquery.countdown.min.js"></script>
-  <script src="js/jquery.magnific-popup.min.js"></script>
-  <script src="js/bootstrap-datepicker.min.js"></script>
-  <script src="js/aos.js"></script>
-
-  <script src="js/main.js"></script>
-
-
-  </body>
-</html>
+  
 <?php
 include "footer1.php";
 ?>
