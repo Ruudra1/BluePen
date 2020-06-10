68dee715b53664a3315e6fbb033aa5c42676ec37
kgami14 <51937509+kgami14@users.noreply.github.com>
2020-06-10 07:24:24 +0530

    minor changes

--- src/navbar1.php
+++ src/navbar1.php
@@ -111,7 +111,7 @@
   </div>
     <nav class="navbar navbar-expand-sm   navbar-light bg-light">
     <div class="col-6 col-md-3 col-xl-3  d-block">
-           <img src="images/logo.png" class="img-fluid" style="width:70%" style="height:70%">
+           <a href="index.php"><img src="images/logo.png" class="img-fluid" style="width:70%" style="height:70%"></a>
           </div>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
@@ -120,7 +120,7 @@
         <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
           <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
             <li class="nav-item">
-              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+              <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="viewclients.php">View Clients</a>
